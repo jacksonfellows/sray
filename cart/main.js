a693883efc43d1299f2c83151d009dec0d8632a8
Jackson Fellows <jtfellows@icloud.com>
2024-03-13 14:09:07 -0400

    Make Cartesian version work more like polar version.

--- cart/main.js
+++ cart/main.js
@@ -48,16 +48,31 @@ function plotVM() {
 	vctx.restore();
 }
 
-function v(z) {
-	let [z0, v0] = VM[0];
-	for (let i = 1; i < VM.length; i++) {
-		let [z1, v1] = VM[i];
-		if (z0 <= z && z < z1) {
-			return v0 + (z - z0)*(v1 - v0)/(z1 - z0);
+function interpVM(dz) {
+	let interp = [];
+	let i = 0;
+	for (let z = 0; z < zMax; z += dz) {
+		while (z >= VM[i][0]) {
+			i++;
 		}
-		z0 = z1; v0 = v1;
+		let [r0, v0] = VM[i - 1];
+		let [r1, v1] = VM[i];
+		interp.push([z, v0 + (z - r0)*(v1 - v0)/(r1 - r0)]);
 	}
-	return v0;
+	interp.push(VM[VM.length - 1]);
+	return interp;
+}
+
+let VMInterp;
+
+function v(z) {
+	if (z < 0) return VMInterp[0][1];
+	if (z > zMax) return VMInterp[zMax][1];
+	let i = Math.floor(z);
+	if (z == i) return VMInterp[i][1];
+	let [r0, v0] = VMInterp[i];
+	let [r1, v1] = VMInterp[i + 1];
+	return v0 + (z - r0)*(v1 - v0)/(r1 - r0);
 };
 
 let C = ["#a8780d", "#a9770f", "#ab7611", "#ae7414", "#af7316", "#b27119", "#b3701b", "#b56f1d", "#b76d20", "#b96c22", "#bb6a25", "#bd6926", "#bf672a", "#c0662b", "#c1652d", "#c46230", "#c56132", "#c75f35", "#c85e37", "#ca5c3a", "#cb5a3c", "#cc593e", "#ce5741", "#cf5643", "#d05347", "#d15249", "#d2514b", "#d44e4f", "#d54d51", "#d64a55", "#d74957", "#d8465c", "#d9455e", "#d94361", "#db4066", "#db3f68", "#dc3c6d", "#dd3a70", "#dd3876", "#de3678", "#de357b", "#de3281", "#df3184", "#df2e8a", "#df2d8d", "#df2a93", "#de2997", "#de289a", "#de27a0", "#dd26a3", "#dc25a9", "#dc25ad", "#db25b0", "#da26b6", "#d926b9", "#d727be", "#d628c1", "#d42ac7", "#d32bc9", "#d22dcc", "#d02fd0", "#cf31d3", "#cc34d7", "#cb35d9", "#c839dd", "#c63adf", "#c53ce1", "#c23fe4", "#c041e5", "#bd45e8", "#bb46e9", "#b948eb", "#b64bed", "#b44dee", "#b050ef", "#ae52f0", "#aa55f1", "#a957f2", "#a759f3", "#a25cf3", "#a05df4", "#9c60f4", "#9a62f4", "#9565f4", "#9366f4", "#9168f4", "#8c6bf3", "#8a6cf3", "#856ff2", "#8270f1", "#7d73f0", "#7b74ef", "#7875ef", "#7378ed", "#7079ec", "#6b7be9", "#687ce8", "#667ee7", "#6080e4", "#5d81e3", "#5883df", "#5584de", "#4f86da", "#4d87d8", "#4a87d7", "#4589d3", "#428ad1", "#3d8bcd", "#3b8ccb", "#368dc7", "#348ec4", "#328ec2", "#2e8fbe", "#2c90bc", "#2891b8", "#2791b6", "#2591b4", "#2392b0", "#2192ae", "#1f93aa", "#1e93a8", "#1c94a4", "#1b94a2", "#1a94a0", "#19959c", "#18959a", "#169696", "#159694", "#149690", "#13978e", "#12978c", "#109788", "#0f9786", "#0d9882", "#0d9880", "#0c987c", "#0b9979", "#0b9977", "#0c9973", "#0d9970", "#0f9a6b", "#119a69", "#139a66", "#179a61", "#199a5e", "#1f9a58", "#219a55", "#279a4f", "#2b9a4c", "#2e9a49", "#359943", "#389940", "#409839", "#439836", "#4b972f", "#4f962c", "#539629", "#5a9423", "#5e9420", "#65921b", "#689119", "#6b9017", "#728f14", "#748e13", "#7a8c11", "#7d8b10", "#828a0f", "#84890e", "#86880e", "#8b860d", "#8d850d", "#91830d", "#93830d", "#97810d", "#99800d", "#9b7f0d", "#9f7d0d", "#a17c0d", "#a47a0d", "#a6790d"];
@@ -98,7 +113,7 @@ function castRay(i_deg) {
 
 	ctx.lineTo(x, z);
 
-	let dz = 0.01;
+	let dz = 0.1;
 
 	if (90 <= i_deg && i_deg <= 270) {
 		dz = -dz;				// Start going up.
@@ -108,25 +123,41 @@ function castRay(i_deg) {
 
 	let N = 100000;
 	for (let n = 0; n < N; n++) {
-		z += dz;
-		let v_z = v(z);
-		let eta = Math.sqrt(1/(v_z*v_z) - p*p);
-		if (isNaN(eta) || eta < 1e-3) {
-			if (dz < 0 && z < 1e-3) break;
+		if (!inBounds(x, z)) break;
+
+		let z2 = z + dz/2;
+		let z3 = z + dz;
+
+		let v1_2 = Math.pow(v(z), 2);
+		let v2_2 = Math.pow(v(z2), 2);
+		let v3_2 = Math.pow(v(z3), 2);
+
+		let eta1 = Math.sqrt(1/v1_2 - p*p);
+		let eta2 = Math.sqrt(1/v2_2 - p*p);
+		let eta3 = Math.sqrt(1/v3_2 - p*p);
+
+		let k1 = p/eta1;
+		let k2 = p/eta2;
+		let k3 = p/eta3;
+
+		let l1 = 1/(v1_2*eta1);
+		let l2 = 1/(v2_2*eta2);
+		let l3 = 1/(v3_2*eta3);
+
+		if (isNaN(k1) || isNaN(k2) || isNaN(k3)) {
+			if (z3 <= 0) break;
 			dz = -dz;
 			continue;
 		}
-		T += 1/(v_z*v_z*eta)*Math.abs(dz);
-		x += p/eta*Math.abs(dz);
-		if (inBounds(x, z)) {
-			ctx.lineTo(x, z);
-		}
-		else {
-			break;
-		}
+
+		x += Math.abs(dz)/6*(k1 + 4*k2 + k3);
+		T += Math.abs(dz)/6*(l1 + 4*l2 + l3);
+		z += dz;
+
+		ctx.lineTo(x, z);
 	}
 
-	if (Math.abs(z) < 1) {
+	if (z <= Math.abs(dz)) {
 		TTs.push([x, T, i_deg]);
 	}
 
@@ -156,6 +187,9 @@ function redraw() {
 
 	// Draw VM.
 	plotVM();
+
+	// Interpolate VM.
+	VMInterp = interpVM(1);
 
 	// Cast rays.
 	TTs = [];
